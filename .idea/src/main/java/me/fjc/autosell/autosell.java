b913f9a0e87ad8fbb5cb1c8691ac57b4f0ffdba9
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-08-30 22:43:28 -0400

    Import array

---
+++ .idea/src/main/java/me/fjc/autosell/autosell.java
@@ -0,0 +1,33 @@
+package main.java.me.fjc.autosell;
+
+import me.gypopo.economyshopgui.api.EconomyShopGUIHook;
+import me.gypopo.economyshopgui.api.events.PostTransactionEvent;
+import me.gypopo.economyshopgui.api.events.PreTransactionEvent;
+import me.gypopo.economyshopgui.api.objects.SellPrices;
+import me.gypopo.economyshopgui.api.prices.AdvancedSellPrice;
+import me.gypopo.economyshopgui.objects.ShopItem;
+import me.gypopo.economyshopgui.util.EcoType;
+import me.gypopo.economyshopgui.util.EconomyType;
+import me.gypopo.economyshopgui.util.Transaction;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.block.DoubleChest;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+
+public class autosell {
+    private EconomyShopGUIHook shopGUIHook;
+
+
+}
