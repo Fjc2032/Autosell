f2bf5b030fbe07fca0c87b02805a9c86bf08cce1
Fjc <155930341+Fjc2032@users.noreply.github.com>
2024-09-30 10:53:35 -0400

    Update API import

--- .idea/src/main/java/me/fjc/ecoapi/EcoAPI.java
+++ .idea/src/main/java/me/fjc/ecoapi/EcoAPI.java
@@ -1,4 +1,13 @@
 package me.fjc.ecoapi
 
 import org.bukkit.entity.Player;
-import org.bukkit.*
+import org.bukkit.*;
+import me.gypopo.economyshopgui.api.EconomyShopGUIHook;
+import me.gypopo.economyshopgui.util.EcoType;
+
+public class EcoAPI implements Listener {
+  public final int version = getVersion();
+
+  private final 
+
+}
